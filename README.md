99c2168eb1724ffa350c3176b1748bf1c7d2458a
Chris Streeter <chris@chrisstreeter.com>
2016-12-15 09:16:03 -0800

    New emoji, from an updated source

--- README.md
+++ README.md
@@ -26,9 +26,9 @@ Run the rake task to build all the snippets inside of the Dash database:
 Emoji
 -----
 
-You can create and update the system-wide emoji snippets in Dash using the `update_emoji.py` script. This script reads the `.plist` file inside of [`Macmoji`](https://github.com/warpling/Macmoji) git submodule and then exports `.toml` files that the `rake dash:build` task reads. Update the submodule and then run `update_emoji.py` to populate the new emoji.
+You can create and update the system-wide emoji snippets in Dash using the `update_emoji.py` script. This script reads the `.json` file inside of [`emojilib`](https://github.com/muan/emojilib) node installed package , and then exports `.toml` files that the `rake dash:build` task reads. `npm install` and then run `update_emoji.py` to populate the new emoji.
 
 Credits
 -------
 
-Code adopted from [@tbpgr](https://github.com/tbpgr)'s [`dash_snippets_builder`](https://github.com/tbpgr/dash_snippets_builder) repository. The idea to search for it came from the [`Macmoji`](https://github.com/warpling/Macmoji) repo, which is a submodule, and where the emoji dictionary comes from.
+Code adopted from [@tbpgr](https://github.com/tbpgr)'s [`dash_snippets_builder`](https://github.com/tbpgr/dash_snippets_builder) repository.
--- update_emoji.py
+++ update_emoji.py
@@ -1,30 +1,58 @@
 #!/usr/bin/env python
 
 import codecs
+import collections
+import json
+import re
 import os
-import plistlib
 
-plist_path = os.path.relpath(
-    os.path.join('.', 'Macmoji', 'emoji substitutions.plist'))
+json_path = os.path.relpath(
+    os.path.join('.', 'node_modules', 'emojilib', 'emojis.json'))
+snippets_path = os.path.join('snippets', 'Emoji')
 
-# Read the plist
-plist = plistlib.readPlist(plist_path)
-
-# For every emoji in the plist, create the snippet
-for emoji in plist:
-    title = emoji['phrase']
-    shortcut = emoji['shortcut']
-
-    filename = '{}.toml'.format(shortcut.replace(':', '').replace('+', ''))
-
-    contents = u"""[snippet]
+CONTENT_FORMAT = u"""[snippet]
 body = "{emoji}"
 syntax = "None"
 tag = "Emoji"
-title = "{shortcut}"
-""".format(emoji=title, shortcut=shortcut)
+title = ":{shortname}:"
+"""
 
-    path = os.path.join('snippets', 'Emoji', filename)
+# Read the json file
+with open(json_path) as f:
+    json_dict = json.loads(f.read())
+
+emoji_keywords = collections.defaultdict(set)
+
+
+def valid_emoji_shortname(shortname):
+    pattern = re.compile(r'^[A-Za-z0-9_\-]+$')
+    return pattern.match(shortname) is not None
+
+
+def write_emoji(title, shortname):
+    if not valid_emoji_shortname(shortname):
+        return
+    filename = u'{}.toml'.format(shortname)
+    contents = CONTENT_FORMAT.format(emoji=title, shortname=shortname)
+
+    path = os.path.join(snippets_path, filename)
 
     with codecs.open(path, 'w', 'utf-8') as f:
         f.write(contents)
+
+
+# For every emoji in the json, create the snippet
+for shortname, emoji in json_dict.items():
+    title = emoji['char']
+
+    for keyword in emoji['keywords']:
+        emoji_keywords[keyword].add(title)
+
+    write_emoji(title, shortname)
+
+# For every keyword, see if there is only one emoji, if so, write it out
+for keyword, emojis in emoji_keywords.items():
+    if len(emojis) != 1:
+        continue
+
+    write_emoji(emojis.pop(), keyword)
