1ed8fbd2302649e7ed6067b268b14663c2b516ab
Chris Streeter <chris@chrisstreeter.com>
2017-03-30 10:06:04 -0700

    Allow new emojis

--- update_emoji.py
+++ update_emoji.py
@@ -6,6 +6,8 @@ import json
 import re
 import os
 
+from slugger import Slugger
+
 json_path = os.path.relpath(
     os.path.join('.', 'node_modules', 'emojilib', 'emojis.json'))
 snippets_path = os.path.join('snippets', 'Emoji')
@@ -17,42 +19,62 @@ tag = "Emoji"
 title = ":{shortname}:"
 """
 
+ALIASES = {
+    'tada': [
+        'celebration',
+    ],
+}
+
+slugger = Slugger(lang='en_US')
+
 # Read the json file
 with open(json_path) as f:
     json_dict = json.loads(f.read())
 
-emoji_keywords = collections.defaultdict(set)
+emoji_keywords = collections.defaultdict(list)
 
 
-def valid_emoji_shortname(shortname):
-    pattern = re.compile(r'^[A-Za-z0-9_\-]+$')
-    return pattern.match(shortname) is not None
+def create_shortname(name):
+    if re.match(r'^[a-zA-Z0-9\+\-\_]+$', name):
+        return name
+
+    return slugger.sluggify(name)
 
 
-def write_emoji(title, shortname):
-    if not valid_emoji_shortname(shortname):
-        return
-    filename = u'{}.toml'.format(shortname)
-    contents = CONTENT_FORMAT.format(emoji=title, shortname=shortname)
+def write_emoji(emoji_symbol, emoji_name):
+    filename = u'{}.toml'.format(emoji_name)
+    contents = CONTENT_FORMAT.format(
+        emoji=emoji_symbol, shortname=emoji_name)
 
     path = os.path.join(snippets_path, filename)
 
-    with codecs.open(path, 'w', 'utf-8') as f:
-        f.write(contents)
+    with codecs.open(path, 'w', 'utf-8') as emoji_file:
+        emoji_file.write(contents)
 
 
-# For every emoji in the json, create the snippet
-for shortname, emoji in json_dict.items():
-    title = emoji['char']
+# Populate the set of keywords
+for shortname, emoji_dict in json_dict.items():
+    emoji = emoji_dict['char']
 
-    for keyword in emoji['keywords']:
-        emoji_keywords[keyword].add(title)
-
-    write_emoji(title, shortname)
-
-# For every keyword, see if there is only one emoji, if so, write it out
-for keyword, emojis in emoji_keywords.items():
-    if len(emojis) != 1:
+    valid_shortname = create_shortname(shortname)
+    if not valid_shortname:
+        print(u'Shortname {} is not a valid emoji title'.format(
+            shortname))
         continue
 
-    write_emoji(emojis.pop(), keyword)
+    # Add the primary to the front of the array, since that is always the one
+    # we want.
+    emoji_keywords[valid_shortname].insert(0, emoji)
+
+    # Add all aliases to the end of the array
+    for keyword in emoji_dict['keywords']:
+        valid_keyword = create_shortname(keyword)
+        if not valid_keyword:
+            print(u'Shortname {} is not a valid emoji keyword'.format(
+                keyword))
+            continue
+        emoji_keywords[valid_keyword].append(emoji)
+
+# For every keyword, write it out.
+for keyword, emojis in emoji_keywords.items():
+    write_emoji(emojis[0], keyword)
